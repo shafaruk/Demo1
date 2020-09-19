2ec24bc80aa34aeca4783ff77d14bd9cb1665be1
shafaruk <sfaruk09@gmail.com>
2020-09-19 17:43:38 -0400

    laranConstructor

---
+++ src/basic/LearnConstructor.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class LearnConstructor {
+
+    public LearnConstructor(){
+        System.out.println("This is the default constructor");
+    }
+
+    public static void main(String[] args) {
+        LearnConstructor obj= new LearnConstructor();
+    }
+}
