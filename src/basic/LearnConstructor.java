ca6ca06ddfdc6f62f8b95964c1dc42b1c229203f
shafaruk <sfaruk09@gmail.com>
2020-09-19 17:56:40 -0400

    constructor

--- src/basic/LearnConstructor.java
+++ src/basic/LearnConstructor.java
@@ -1,12 +1,22 @@
 package basic;
 
 public class LearnConstructor {
+    String name;
+    int age;
 
     public LearnConstructor(){
         System.out.println("This is the default constructor");
+
+    }
+    public LearnConstructor(String name, int age){
+        this.name=name;
+        this.age=age;
+        System.out.println("My name is "+ name + " and my age is "+age);
+
     }
 
     public static void main(String[] args) {
         LearnConstructor obj= new LearnConstructor();
+        LearnConstructor obj1=new LearnConstructor("faruk",25);
     }
 }
