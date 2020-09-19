aa2a7922a67522b4abc73e6ec65446f131984874
shafaruk <sfaruk09@gmail.com>
2020-09-19 17:29:31 -0400

    second commit

--- src/basic/LearnJavaBasic.java
+++ src/basic/LearnJavaBasic.java
@@ -3,6 +3,21 @@ package basic;
 public class LearnJavaBasic {
 
     public static void main(String[] args) {
-        System.out.println(" Hello world");
-    }
+        // control flow
+
+        // if else condition () condition
+        //int number1=25;
+        String gender ="male";
+        int age =18;
+
+
+
+        if (age >=18 || age < 21){
+            System.out.println("You can buy alcohols");
+
+        }else{
+            System.out.println(" You are not allowed to buy");
+        }
+
+}
 }
